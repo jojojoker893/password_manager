f14e15040ff35ecb8ffb8d3b3dc8a1a9dbbaeb0e
jojojoker <jojojoker114514@gmail.com>
2024-08-02 20:29:33 +0900

    パスワードマネージャーの作成
    GetPasswordの条件分岐を修正予定

---
+++ password_manager.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+while true
+do
+    echo "パスワードマネージャーへようこそ！"
+    echo "次の選択肢から入力してください(Add Password/Get Password/Exit)"
+    read choices
+
+    case $choices in
+            "Add Password")
+                echo "サービス名を入力してください："
+                read service
+
+                echo "ユーザー名を入力してください："
+                read username
+
+                echo "パスワードを入力してください："
+                read password
+
+                echo " $service:$username:$password " >> passwordlist.txt
+                echo "パスワードの追加は成功しました。" ;;
+
+            "Get Password")
+                echo "サービス名を入力してください:"
+                read service 
+                
+                if ! grep -q "$service" passwordlist.txt;then
+                    echo "そのサービス登録されていません。"
+                else
+                    "aaaa"
+                fi
+                ;;
+
+            "Exit")
+                echo "Thank you!"
+                break ;;
+            
+            *)
+                echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。" ;;
+    esac
+done
